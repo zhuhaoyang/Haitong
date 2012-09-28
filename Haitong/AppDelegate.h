cc44ff6262c414e58f768cc8f86e599360bcddfd
Myth <myth@localhost.(none)>
2012-09-28 14:32:13 +0800

    Initial Commit

---
+++ Haitong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Haitong
+//
+//  Created by Myth on 12-9-28.
+//  Copyright (c) 2012年 Myth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ Haitong/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Haitong
+//
+//  Created by Myth on 12-9-28.
+//  Copyright (c) 2012年 Myth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
